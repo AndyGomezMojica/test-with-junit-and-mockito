b61eabd6c9c9d979d70a1cb7a036acc246b4c0a1
andy <andygomezmojica@gmail.com>
2024-05-23 15:27:20 -0600

    Erase unnecesary class and method

--- src/main/java/com/andygomez/unic_test/web/model/UserInput.java
+++ src/main/java/com/andygomez/unic_test/web/model/UserInput.java
@@ -1,12 +1,9 @@
 package com.andygomez.unic_test.web.model;
 
-import com.andygomez.unic_test.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -14,19 +11,8 @@ import java.util.Date;
 @Builder
 public class UserInput {
 
-    private static final String ADMIN = "ADMIN";
-
     private String name;
     private String lastName;
     private String email;
     private String password;
-
-    public UserInput userToInput(User user){
-        return UserInput.builder()
-                .name(user.getName())
-                .lastName(user.getLastName())
-                .email(user.getEmail())
-                .password(user.getPassword())
-                .build();
-    }
 }
